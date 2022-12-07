c06e4212b08548ce2f6f275cedab3f5e5f4aebe1
Никита <lavrentivev.nikita@gmail.com>
2022-12-07 06:29:41 +0300

    Start

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,51 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+    }
+
+    public static void task1() {
+
+        System.out.println("\nЗадача_1");
+    }
+
+    public static void task2() {
+
+        System.out.println("\nЗадача_2");
+    }
+
+    public static void task3() {
+
+        System.out.println("\nЗадача_3");
+
+    }
+
+    public static void task4() {
+
+        System.out.println("\nЗадача_4");
+
+    }
+
+    public static void task5() {
+
+        System.out.println("\nЗадача_5");
+
+    }
+
+    public static void task6() {
+
+        System.out.println("\nЗадача_6");
+
+    }
+
+    public static void task7() {
+
+        System.out.println("\nЗадача_7");
+
     }
 }
